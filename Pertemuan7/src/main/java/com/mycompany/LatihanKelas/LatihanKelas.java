99f998194329e81761407c09932540a4598e80fd
A-14 <A-14@A-14>
2024-05-27 11:07:20 +0700

    UML dan metode

--- Pertemuan7/src/main/java/com/mycompany/LatihanKelas/LatihanKelas.java
+++ Pertemuan7/src/main/java/com/mycompany/LatihanKelas/LatihanKelas.java
@@ -28,15 +28,20 @@ public class LatihanKelas {
         buku1.author.nama = "Muhammad";
         buku2.judul = "Java 2";
         buku2.author.nama = "Ridwan";
+        Harga hargabaru = new Harga();
         
         System.out.println(buku1.judul);
-        System.out.println(buku1.author);
-//      buku1.author.sebutNama();  
+        buku1.author.sebutNama();  
         System.out.println(buku2.judul);
         System.out.println(buku2.author);
         System.out.println(buku1.tahunpenerbitan);
+        buku1.harga.setJual(120000);
+        System.out.println(buku1.harga.getBeli());
         System.out.println(buku1.harga.beli);
         System.out.println(buku1.penerbitan.nama);
+        System.out.printIn("Harga jual buku 1 = " + buku1.harga.getJual());
+        System.out.printIn("Harga jual buku 2 = " + buku1.harga.getJual());
+        buku1.terjual();
     }
 }
 
@@ -46,9 +51,25 @@ class Buku{
     int tahunpenerbitan = 2024;
     Harga harga = new Harga();
     PerusahaanBuku penerbit = new PerusahaanBuku();
+    
+    public void terjual(){
+        System.out.println("Buku terjual dengan harga : " + harga.getBeli());
+    }
 }
 
 class Harga{
     double jual = 100000.00;
     double beli = 80000.00;
+    
+    public double getBeli() {
+        return beli;
+    }
+    
+    public void setJual(double jual) {
+        this.jual = jual;
+    }
+    
+    public double getJual() {
+        return beli;
+    }
 }
